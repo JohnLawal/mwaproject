f31a42201c3ba1483eed08f81c9434e71bd5a5cb
jilawal@mum.edu <my.github.password>
2019-09-22 17:10:27 -0500

    Updated Packages CRUD

--- serverside/modules/services/packages/crudpackages.js
+++ serverside/modules/services/packages/crudpackages.js
@@ -1,33 +1,59 @@
-const packageSchema = require('../../respository/package')
+const PackageSchema = require('../../respository/package')
+let handlers = {}
 
-let viewPackagesHandler = function(req, res, next) {
-    packageSchema.find((err, documents) => {
+
+handlers.viewPackagesHandler = function(req, res, next) {
+    PackageSchema.find((err, documents) => {
         if (err) return next(Error(err));
 
         res.status(200).json(documents)
     })
 }
 
-let viewOnePackageHandler = function(req, res, next) {
+handlers.viewOnePackageHandler = function(req, res, next) {
     let packageId = req.params.id;
-    packageSchema.findById(packageId, (err, document) => {
+    PackageSchema.findById(packageId, (err, document) => {
         if (err) return next(Error(err));
         res.status(200).json(document)
     })
 }
 
-let createPackageHandler = function(req, res, next) {
-    let newPackage = req.body;
+handlers.createPackageHandler = function(req, res, next) {
+    let newPackage = new PackageSchema(req.body)
+    newPackage.save(function(err) {
+        if (err) return next(Error(err));
+        res.status(201).json({
+            status: 'Success',
+            data: 'Package has been saved'
+        })
+    });
 }
 
-let updatePackageHandler = function() {}
-let deletePackageHandler = function() {}
-
-
-module.exports = {
-    viewPackagesHandler: viewPackagesHandler,
-    viewOnePackageHandler: viewOnePackageHandler,
-    createPackageHandler: createPackageHandler,
-    updatePackageHandler: updatePackageHandler,
-    deletePackageHandler: deletePackageHandler
+handlers.updatePackageHandler = function(req, res, next) {
+    let packageId = req.params.id;
+    let docsModified = PackageSchema.updateOne({ _id: packageId }, req.body)
+    if (docsModified > 0) {
+        res.status(202).json({
+            status: 'Success',
+            data: 'Package has been updated'
+        })
+    } else {
+        res.status(409).json({
+            status: 'Failure',
+            data: 'No package was updated'
+        })
+    }
 }
+
+handlers.deletePackageHandler = function(req, res, next) {
+    let packageId = req.params.id;
+    Tank.deleteOne({ _id: packageId }, function(err) {
+        if (err) return next(Error(err));
+        res.status(202).json({
+            status: 'Success',
+            data: 'Package has been deleted'
+        })
+    });
+}
+
+module.exports = handlers;
