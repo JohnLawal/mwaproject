f01467428df242def75a037fa93bc7a2884f63ed
Dawit Woldegiorgis <dawithw@gmail.com>
2019-09-22 15:44:21 -0500

    service folder created

---
+++ serverside/modules/services/investor.js
@@ -0,0 +1,13 @@
+const service = module.exports;
+
+service.getInverstorById = function (req, res, next){
+
+}
+
+service.getInverstorById = function (req, res, next){
+
+}
+
+service.getInverstorById = function (req, res, next){
+
+}
