defb3afa4cc7045394aa04eb5b93791532d756cb
Dawit Woldegiorgis <dawithw@gmail.com>
2019-09-22 15:35:41 -0500

    removed unnecessary code

--- serverside/modules/respository/investor.js
+++ serverside/modules/respository/investor.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const client = new mongoClient("mongodb+srv://yvan:yvan@cluster0-vkamr.mongodb.net/farminvest?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true});
-
 const investorSchema = new mongoose.Schema({
 firstName : { type :String, required: true},
 lastName : { type :String, required: true},
