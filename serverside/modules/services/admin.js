85edaf535d18ebd6ff076c1cbfd2437945464939
Hagenimana Yvan remy <hagenimanayvanremy@MacBook-Pro-3.local>
2019-09-23 11:50:31 -0500

    add functionalities in router.js and middleware

--- serverside/modules/services/admin.js
+++ serverside/modules/services/admin.js
@@ -28,6 +28,7 @@ let saveAdmin = function(request,response,next){
           response.status(200).json({ message: "successfully", key: token });
         
     } catch (error) {
+      //  next(new Error(error))
         response.status(400).json({status : 400, message : 'Error occured, '+error});
     }
  
