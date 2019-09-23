299b463e2b7d86c0ff569c8e5199fb7f6d7c1fdf
jilawal@mum.edu <my.github.password>
2019-09-23 13:06:58 -0500

    Updated Angular module

--- clientside/src/app/angular-material/angular-material.module.ts
+++ clientside/src/app/angular-material/angular-material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
-  imports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatInputModule],
+  exports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatInputModule],
 })
 export class AngularMaterialModule { }
