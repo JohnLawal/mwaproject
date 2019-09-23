fcf916f01fb8dcc67bfb694644b8c7bc493f9ea6
Dawit Woldegiorgis <dawithw@gmail.com>
2019-09-23 18:02:54 -0500

    add validation to signin

--- clientside/src/app/signin/signin.component.ts
+++ clientside/src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,28 +7,66 @@ import { Component, OnInit, OnChanges } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
-
-  public username: string = '';
-  public password: string = '';
-  
   public credentials = {
     "username" : "dawit",
     "password" : "pass123"
   }
 
-  constructor() { }
+  public loginForm : FormGroup;
+  public username : AbstractControl;
+  public password : AbstractControl;
+
+  constructor(public formBuilder : FormBuilder) {
+    this.loginForm = formBuilder.group({
+      "username" : ['',Validators.required],
+      "password" : ['',Validators.required]
+    })
+    this.username = this.loginForm.get('username');
+    this.password = this.loginForm.get('password');
+   }
 
   login(){
-    console.log("usernmae: " + this.username + "\npassword: " + this.password);
-
+    // console.log("usernmae: " + this.username + "\npassword: " + this.password);
+    this.validateUsername()
+    if(!this.username.invalid){
+      console.log("username is valid");
+      this.validatePassword();
+    }
+      
   }
 
-  badUsername() {
-
+  badUsername() : string {
+    if(this.username.hasError('required'))
+      return "Username is required";
+    else if (this.username.hasError('invalid'))
+      return "The username does not exist";
+    else
+      return "";
   }
 
-  badPassword() {
-    
+  badPassword() : string {
+    if(this.password.hasError('required'))
+      return "Password is required";
+    else if (this.password.hasError('invalid'))
+      return "Incorrect password";
+    else
+      return "";
+  }
+
+  validateUsername() : boolean {
+    if (!(this.username.value == this.credentials.username)){
+      this.username.setErrors({'invalid':true})
+      return false;
+    }
+    return true;
+  }
+
+  validatePassword() : boolean {
+    if (!(this.password.value == this.credentials.password)){
+      this.password.setErrors({'invalid':true})
+      return false;
+    }
+      return true;
   }
 
 
