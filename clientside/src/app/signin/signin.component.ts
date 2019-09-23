a83db9b8d2d5c25c6b108842a2aa86be23c16ef4
Dawit Woldegiorgis <dawithw@gmail.com>
2019-09-23 14:25:42 -0500

    basic signin ui

--- clientside/src/app/signin/signin.component.ts
+++ clientside/src/app/signin/signin.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent {
+
+  public username: string = '';
+  public password: string = '';
+  
+  public credentials = {
+    "username" : "dawit",
+    "password" : "pass123"
+  }
 
   constructor() { }
 
-  ngOnInit() {
+  login(){
+    console.log("usernmae: " + this.username + "\npassword: " + this.password);
+
   }
+
+  badUsername() {
+
+  }
+
+  badPassword() {
+    
+  }
+
+
 
 }
