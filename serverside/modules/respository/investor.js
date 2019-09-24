7573be0ac62fbc6025dbf909a2ce3c12e0b6d408
Dawit Woldegiorgis <dawithw@gmail.com>
2019-09-24 00:16:58 -0500

    sparsed username index for investor

--- serverside/modules/respository/investor.js
+++ serverside/modules/respository/investor.js
@@ -5,7 +5,7 @@ mongoose.connect("mongodb+srv://yvan:yvan@cluster0-vkamr.mongodb.net/farminvest?
 const investorSchema = new mongoose.Schema({
 firstName : { type :String, required: true},
 lastName : { type :String, required: true},
-username : { type :String, required: true, unique :true, index:true},
+username : { type :String, required: true, unique :true, index:true, sparse},
 email : { type :String, required: true, unique :true},
 password : { type :String, required: true},
 accountNumber : { type :String, unique :true},
