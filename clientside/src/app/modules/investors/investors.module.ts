3624caa2ed37c9bc44db4ea1427e26429aa78e63
jilawal@mum.edu <my.github.password>
2019-09-23 20:05:59 -0500

    Setup investor Backend structure

--- clientside/src/app/modules/investors/investors.module.ts
+++ clientside/src/app/modules/investors/investors.module.ts
@@ -1,12 +1,35 @@
-import { NgModule } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import { NgModule} from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
+import {AngularMaterialModule} from '../../angular-material/angular-material.module';
+import {ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { InvestorsComponent } from './investors.component';
 
-
+const routes: Routes = [
+  {path: '', component: InvestorsComponent,
+  children: [
+    // {path: 'dashboard' , component: SignupComponent},
+    // {path: 'myinvestments', component: SigninComponent},
+    // {path: 'packages', component: AboutComponent},
+    // {path: 'packages/:id', component: ForgotpasswordComponent},
+    // {path: 'myprofile', component: ResetpasswordComponent},
+    // {path: 'myinvestments', component: ResetpasswordComponent},
+    // {path: 'myfollowedfarms', component: ResetpasswordComponent},
+    // {path: 'contactus', component: ResetpasswordComponent},
+    // {path: 'buy/:id', component: ResetpasswordComponent}
+  ]}
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [InvestorsComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [MediaMatcher]
 })
-export class InvestorsModule { }
+export class InvestorsModule {
+}
