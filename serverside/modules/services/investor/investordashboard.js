f36e5c5be25aa4d45e48491b6ba33a21947b60f0
jilawal@mum.edu <my.github.password>
2019-09-24 16:09:25 -0500

    Updated Investor Dashboard

--- serverside/modules/services/investor/investordashboard.js
+++ serverside/modules/services/investor/investordashboard.js
@@ -6,34 +6,40 @@ let handlers = {}
 
 
 handlers.getDashboardStats = async function(req, res, next) {
-    //get users id
-    //the get invesments in packages where the username is this user's
-    const username = 'Tester';
-    let user = await InvestorSchema.find({ username: username })
-    user = user[0];
-    let userInvestments = user.investedPackages;
 
-    let sum = await PackagesSchema.aggregate([{ $match: { status: "Available" } }, { $group: { _id: null, total: { $sum: "$amount" } } }]);
+    try {
+        //get users id
+        //the get invesments in packages where the username is this user's
+        const username = 'Tester';
+        let user = await InvestorSchema.find({ username: username })
+        user = user[0];
+        let userInvestments = user.investedPackages;
 
-    let totalUnitsAvailable = sum.length > 0 ? sum[0] : 0;
+        let sum = await PackagesSchema.aggregate([{ $match: { status: "Available" } }, { $group: { _id: null, total: { $sum: "$amount" } } }]);
 
-    const numberOfInvestments = userInvestments.length;
-    let numberOfFarmsFollowed = user.followedPackages.length;
+        let totalUnitsAvailable = sum.length > 0 ? sum[0] : 0;
 
-    let mostRecentInvestment = userInvestments.pop();
-    if (!mostRecentInvestment) {
-        mostRecentInvestment = "None"
+        const numberOfInvestments = userInvestments.length;
+        let numberOfFarmsFollowed = user.followedPackages.length;
+
+        let mostRecentInvestment = userInvestments.pop();
+        if (!mostRecentInvestment) {
+            mostRecentInvestment = "None"
+        }
+
+        res.status(200).json({
+            status: "Success",
+            data: {
+                mostRecentInvestment: mostRecentInvestment,
+                totalUnitsAvailable: totalUnitsAvailable,
+                numberOfInvestments: numberOfInvestments,
+                numberOfFarmsFollowed: numberOfFarmsFollowed
+            }
+        })
+    } catch (err) {
+        next(Error(err))
     }
 
-    res.status(200).json({
-        status: "Success",
-        data: {
-            mostRecentInvestment: mostRecentInvestment,
-            totalUnitsAvailable: totalUnitsAvailable,
-            numberOfInvestments: numberOfInvestments,
-            numberOfFarmsFollowed: numberOfFarmsFollowed
-        }
-    })
 }
 
 module.exports = handlers;
