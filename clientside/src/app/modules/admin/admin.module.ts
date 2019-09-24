5860aff0a1ee49132eb90c9ca7583ec409a35f62
jilawal@mum.edu <my.github.password>
2019-09-24 00:45:55 -0500

    Setup structure for admin backend

--- clientside/src/app/modules/admin/admin.module.ts
+++ clientside/src/app/modules/admin/admin.module.ts
@@ -1,12 +1,42 @@
+import {MediaMatcher} from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes, RouterModule } from '@angular/router';
+import {AngularMaterialModule} from '../../angular-material/angular-material.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import { AdminComponent } from './admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PackagesComponent } from './packages/packages.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { MembersComponent } from './members/members.component';
+import { OfflinepaymentsComponent } from './offlinepayments/offlinepayments.component';
+import { ViewmemberComponent } from './viewmember/viewmember.component';
+import { ViewpackageComponent } from './viewpackage/viewpackage.component';
 
-
+const routes: Routes = [
+  {path: '', component: AdminComponent,
+  children: [
+    {path: '' , component: DashboardComponent},
+    {path: 'dashboard' , component: DashboardComponent},
+    {path: 'investments', component: InvestmentsComponent},
+    {path: 'packages', component: PackagesComponent},
+    {path: 'packages/:id', component: ViewpackageComponent},
+    {path: 'members', component: MembersComponent},
+    {path: 'members/:id', component: ViewmemberComponent},
+    {path: 'offlinepayments', component: OfflinepaymentsComponent},
+  ]}
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent, DashboardComponent,
+     PackagesComponent, InvestmentsComponent,
+      MembersComponent, OfflinepaymentsComponent, ViewmemberComponent, ViewpackageComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [MediaMatcher]
 })
 export class AdminModule { }
---
+++ clientside/src/app/modules/admin/viewmember/viewmember.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewmember',
+  templateUrl: './viewmember.component.html',
+  styleUrls: ['./viewmember.component.css']
+})
+export class ViewmemberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
