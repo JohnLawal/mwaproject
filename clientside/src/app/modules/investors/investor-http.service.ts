a89481a8acdaf98de1c63bca30570bc64e374ce0
jilawal@mum.edu <my.github.password>
2019-09-25 00:23:58 -0500

    Completed Investor View Packages

--- clientside/src/app/modules/investors/investor-http.service.ts
+++ clientside/src/app/modules/investors/investor-http.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 export class InvestorHttpService {
   private getDashboardStatsLink = 'http://localhost:2020/api/v1/invest/dashboard';
   private getMyInvestmentsLink = 'http://localhost:2020/api/v1/invest/investments';
-  // private getAllPackagesLink = 'http://localhost:2020/api/v1/protected';
+  private getAllPackagesLink = 'http://localhost:2020/api/v1/packages';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -17,8 +17,8 @@ export class InvestorHttpService {
   getAllInvestments(): Observable<any> {
     return this.httpClient.get(this.getMyInvestmentsLink);
   }
-  // getAllPackages(): any {
-  //   return this.httpClient.get(this.getAllPackagesLink);
-  // }
+  getAllPackages(): any {
+    return this.httpClient.get(this.getAllPackagesLink);
+  }
 
 }
--- clientside/src/app/modules/investors/packages/packages.component.ts
+++ clientside/src/app/modules/investors/packages/packages.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { InvestorHttpService } from './../investor-http.service';
+
+
+
+export interface Package {
+  '_id': string;
+  'name': string;
+  'amount': number;
+  'contractPeriod': number;
+  'expectedReturn': number;
+  'units': number;
+  'description': string;
+  'status': string;
+}
 
 @Component({
   selector: 'app-packages',
@@ -6,10 +21,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./packages.component.css']
 })
 export class PackagesComponent implements OnInit {
+  packages: Package[];
 
-  constructor() { }
+  constructor(private router: Router, private investorHttpService: InvestorHttpService  ) { }
 
   ngOnInit() {
+    this.investorHttpService.getAllPackages().subscribe(
+      (response: any) => {
+        this.packages = response;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
   }
 
 }
+
